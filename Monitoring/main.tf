4f62a6170ddb5a47bacbfa3f3fd9325e2c7c53a3
CDV-KK <kkesy1@edu.cdv.pl>
2023-05-20 09:45:23 +0200

    create logic app to start vms

--- Monitoring/main.tf
+++ Monitoring/main.tf
@@ -1,6 +1,6 @@
 module "iaas_nginx" {
-    source = "./applications/iaas_nginx"
+  source = "./applications/iaas_nginx"
 
-    datadog_api_key = var.datadog_api_key
-    datadog_app_key = var.datadog_app_key
+  datadog_api_key = var.datadog_api_key
+  datadog_app_key = var.datadog_app_key
 }
